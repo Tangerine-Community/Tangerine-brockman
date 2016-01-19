8c7423772d900eae06e320c65acf7a23f0bc0af8
Josh Anderson <fetmar@gmail.com>
2016-01-19 01:05:01 -0500

    add paths with rvm

--- server-init.sh
+++ server-init.sh
@@ -14,6 +14,7 @@ else
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   curl -sSL https://get.rvm.io | bash -s stable
   sudo usermod -a -G rvm `whoami`
+  source /home/`whoami`/.rvm/scripts/rvm
 
   # set secure path options
   if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
